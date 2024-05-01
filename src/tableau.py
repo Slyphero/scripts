95177ad05698f5fb170bd6e98e18dcbcff7c7a48
Raphaël Heng <raphael.heng2@gmail.com>
2024-05-01 12:03:46 +0200

    amelioration script

--- src/tableau.py
+++ src/tableau.py
@@ -1,3 +1,5 @@
+import textwrap
+
 def print_tableau():
     saisie = input("Saisir données : ")
     saisie_splited = saisie.split()
@@ -18,15 +20,32 @@ def print_tableau():
                 text = text + " " + saisie_splited[i]
     elts.append(text)
 
-    print(('\\begin{center}\n'
-        '\\begin{tabular}{|c||c||c||c|c|}\n'
-        '\\hline\n'
-        f'{elts[0]} & {elts[1]} & {elts[2]} & {elts[3]} & {elts[4]} \\\\\n'
-        '\\hline\n'
-        f'{elts[5]} & {elts[6]} & {elts[7]} & {elts[8]} & {elts[9]} \\\\\n'
-        '\\hline\n'
-        '\\end{tabular}\n'
-        '\\end{center}\n'))
+    message_string = r'{sans_vote} & {abstentions} & {contres} & {pour} & {message} \\'
+
+    # Fonction anonyme formattant la string de message
+    message_formatter = lambda sans_vote, abstentions, contres, pour, message: message_string.format(
+        sans_vote=sans_vote, abstentions=abstentions, contres=contres, pour=pour, message=message)
+
+    # Formule LaTeX pour le tableau
+    formula_string = textwrap.dedent(r'''
+    \begin{center}
+    \begin{tabular}{|c||c||c||c|c|}
+    \hline
+    $$1$$
+    \hline
+    $$2$$
+    \hline
+    \end{tabular}
+    \end{center}
+    ''')
+
+    # Remplace dans la formule du tableau les caractères spéciaux assignés pour ça
+    # par un message formatté
+    formatted_string = formula_string.replace('$$1$$', message_formatter(*elts[0:5]))
+    formatted_string = formatted_string.replace('$$2$$', message_formatter(*elts[5:]))
+
+    # Affiche la forule LaTeX formattée
+    print(formatted_string)
     
 while True:
     print_tableau()
